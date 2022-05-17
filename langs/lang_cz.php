42faf48baae83e086d4421a945b64686b08c6aef
Jonáš Židek <55092714+Johno95CZ@users.noreply.github.com>
2022-05-17 21:59:25 +0200

    Přidané soubory

---
+++ langs/lang_cz.php
@@ -0,0 +1,44 @@
+<?php
+
+define("_NAV", "HOME");
+define("_NAV2", "O NÁS");
+define("_NAV3", "CANSAT");
+define("_NAV4", "NÁŠ SATELIT");
+define("_NAV5", "NÁŠ TEAM");
+define("_NAV6", "NOVINKY");
+define("_NAV7", "PARTNEŘI");
+define("_NAV8", "LIVE (BETA)");
+define("_NAV9", "GALERIE");
+
+define("_TEXT1", "Název PASPAL byl vytvořen spojením slov Private Association Studying Planets Appropriate (for) Life.  Tento název byl prvně doplněn sloganem PASPAL letí do Vesmíru! Jelikož tento rok bychom se chtěli opravdu dostat výše než jen 1 km na zemí, tak se vytvořil nový slogan. PASPAL letí do Stratosféry! Celý náš postup a pokroky můžete sledovat na našich sociálních sítích nebo si domluvit přímo schůzku s našim týmem. Budeme moc rádi, když se s Vámi setkáme. ");
+define("_TEXT2", "Tým PASPAL tvoří šest studentů Střední průmyslové školy POJ-FM, která se nachází ve Frýdku-Místku. Náš Tým jsme vytvořili ke konci roku 2020, kde jsme začali vytvářet první prototyp družice a zapojili jsme se do soutěže CanSat 2020/2021. Z počátku kvůli korona opatření jsme se scházeli do semifinále online. Semifinále bylo online, proto jsme museli vytvořit pouze prezentaci a splnit určité podmínky. Postoupili jsme do finále, kde jsme předvedli naší družici s názvem SpaceCan. Naše zařízení muselo umělo a umí měřit teplotu, tlak, GPS. My jsme doplnili také metan a solární panely okolo družice. Tento projekt v soutěži jsme nazvali Zkoumání podmínek pro život organismů. Tento rok jsme se zapojili po druhé do soutěže CanSat. Více informací o naší nové družici naleznete níže. ");
+
+define("_DATA1", "Partnerů");
+define("_DATA2", "Úspěšných testů");
+define("_DATA3", "Řádků kódů");
+define("_DATA4", "Počet pokusů (SpaceCan)");
+define("_DATA5", "Počet pokusů (SpaceCan2)");
+
+define("_TEXT3", "Co je soutěž CanSat");
+define("_TEXT4", "CanSat je malý meteorologický satelit o velikosti plechovky. Tuto studentskou soutěž pořádá vzdělávací kancelář Evropské kosmické agentury ESERO. Tým má za úkol sestrojit napodobeninu skutečného satelitu, který má rozměry ve velikosti plechovky a také by měl postupovat, jako skutečný satelit. Do velikosti plechovky se musí vejít všechny řídící jednotky, senzory až po baterie a napájení. Týmy musí pomocí satelitu měřit tři základní informace jako GPS lokaci, teplotu a atmosférický tlak. Také musí zvolit sekundární misi (v našem případě měření methanu, oxidu uhličitého a kyslíku). Hlavním cílem je naměřená data analyzovat. Data budou odesílána do pozemní stanice, kde se vyhodnotí a případně zasadí do širšího kontextu celé mise. Satelit bude vynesen dronem popř. letadlem do výšky 1km a musí bezpečně dopadnou, aby byl využit pro další analyzování dat která budou uložena na SD kartě. Na závěr musí týmy celou misi odprezentovat odborné porotě. Nakonec soutěže budeme moci vyslat naší upravenou družici do Stratosféry. ");
+
+define("_TEXT5", "Naší družici jsme pojmenovali SpaceCan. Znamená to, že poletíme možná do Vesmíru. Naši další družici, která poletí až do stratosféry a bude mít vnitřek od první družice s vytápěním u utěsněním jsme nazvali StratoCan. Chtěli bychom také podpořit náš bývalý tým školy POJFM a také se tento název velice hodí k našemu cíli. S touto družicí určitě doletíme do Stratosféry. SpaceCan musí mít určité povinné věci, které musí být hotové do semifinále. Tzn. Musí měřit teplotu, tlak, GPS a komunikovat přes anténu na 1 km. Jako sekundární misi dodáme této družici telefon, který bude komunikovat s námi a posílat data rovnou na web. Chtěli bychom tuto družici zautomatizovat a data, která uvidíme rovnou na webu, tak se nám automaticky vloží do grafů a prezentace. Jelikož máme telefon, tak bychom také chtěli využít obě kamery, které bychom nám mohli udělat záznam z letu. U družice StatoCan bychom chtěli vnitřek SpaceCan využít, akorát obal a těsnící materiál bude lepší, jelikož ve Stratosféře je až -75 °C. Nakonec, aby se nám baterie nevybila, tak na celý padák dáme solární panely. S těmito dvěma satelity bychom chtěli dostat do Evropského kola.  ");
+
+define("_TEXT6", "SEZNAM ČLENŮ");
+define("_TEAM1", "Team Captain, Management");
+define("_TEAM4", "Programování");
+define("_TEAM5", "Záznam dat");
+define("_TEAM6", "3D tisk");
+define("_TEAM10", "Grafika");
+define("_TEAM11", "Anténa");
+define("_TEAM12", "Padákový systém");
+
+define("_TEXT7", "Vše ze sociálních sítí");
+
+define("_TEXT8", "Děkujeme");
+
+define("_TEXT9", "GALERIE");
+
+define("_BUTTON1", "Zpět nahoru");
+define("_VERSION", "Verze");
+define("_TELCISLO", "Tel. Číslo");
